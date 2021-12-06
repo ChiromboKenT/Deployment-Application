26304629f90dddcbc1ca4b39740f2a688c37ad8b
Kenny Chirombo <57112312+ChiromboKenT@users.noreply.github.com>
2021-12-06 20:42:32 +0200

    fix: Make Deployment app Ready to run

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const axios = require("axios");
 const http = require("http");
 const path = require('path')
 const config = require("./config")
-const fsp = require('fs/promises');
 const fs = require('fs')
 const zipdir = require('zip-dir')
-const FormData = require('form-data');
+const request=require('request')
+const npm = require('npm-programmatic');
+
+
 
 const remote = `https://${config.GIT_USER}:${config.GIT_PASS}@${config.GIT_URL}`;
 
@@ -24,47 +26,60 @@ if (!fs.existsSync(dir)){
     fs.mkdirSync(dir, { recursive: true });
 }
 
-const Server =http.createServer((req, res) =>  {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(config),'utf-8'); //end the response
-  })
-  
-Server.listen(config.PORT, () => console.log(`Listening on: ${config.PORT}`));
-
-Server.on('connection', async (err,stream) => {
-    console.log("Someone Connected!!")
+const main = async  () => {
+  console.log("Someone Connected!!")
     try{
         if(!fs.existsSync(path.join(dir,"candidate-test-nodejs-2021"))){
-            const cloneResult =  await git.clone(remote)
+            const cloneResult =  await git.clone(remote,["--single-branch","--branch","Kenny"])
+            
+           
+            
         }
-        
+        const packageBuffer = JSON.parse(fs.readFileSync(path.join(dir,"candidate-test-nodejs-2021","package.json"),'utf-8'))
+        const dependencies = Object.keys(packageBuffer['dependencies']) || []
+        const InstalledPackagesList = await npm.install(dependencies, {
+          cwd:`${path.join(dir,"candidate-test-nodejs-2021")}`,
+          output:true
+      }
+
+        )
+        console.log(InstalledPackagesList)
         const buffer = await zipdir(`${path.join(dir,"candidate-test-nodejs-2021")}`, { saveTo: `${path.join(dir,"candidate-test-nodejs-2021.zip")}`,each: p => console.log(p, "added!") },  async (err, buffer) => 
         {   
             if(err){
                 throw new Error(err)
             }
             const fileStream = fs.createReadStream(`${path.join(dir,"candidate-test-nodejs-2021.zip")}`);
-           
-            const result = await axios({
-                headers: { "Content-Type": "application/x-zip-compressed" },
-                method: "post",
-                url: `${config.AZ_URL}`,
-                auth: {
-                  username: config.AZ_USER,
-                  password: config.AZ_PASS
-                },
-                data: fileStream
-              });
-            console.log(`Result: ${result} `)
-        });
+            const options = {
+              method: "POST",
+              url: `${config.AZ_URL}`,
+              auth: {
+                'user': config.AZ_USER,
+                'pass': config.AZ_PASS
+              },
+              body: fileStream
+            }
+            console.log(options.url)
+            const result = () => {
+              return new Promise((resolve,reject) => {request.post(options, (error, response) => {
+                if(error){
+                  reject(error)
+                }
+                resolve(response)
+              })})
+            }
+            const call = await result()
+            console.log(call)
+          });
        
 
     }catch(err){
         console.log(err)
     }
-})
-  process.on("unhandledRejection", err => {
-    console.log(err);
-    process.exit(1);
-  });
-  
+}
+main()
+ process.on("unhandledRejection", err => {
+  console.log(err);
+  process.exit(1);
+});
+
